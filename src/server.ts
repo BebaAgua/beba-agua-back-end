e359555cce25d594d326e68c2b5f59699303ebd2
Luiz Guilherme Anduze <l.g.anduze@gmail.com>
2023-04-10 12:41:08 -0300

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import "express-async-errors";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
